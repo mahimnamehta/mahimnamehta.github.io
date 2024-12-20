9867dba9e059a46bf8627708826c07794c06f996
Mahimna M Mehta <65457672+mahimnamehta@users.noreply.github.com>
2024-12-20 15:35:22 -0500

    final

---
+++ README.md
@@ -0,0 +1,24 @@
+## Installation
+1. Clone the repository
+    ```bash 
+    git clone https://github.com/dsbalico/dsfolio-v1.git
+    ```
+2. Change into the project directory
+    ```bash
+    cd dsfolio-v1
+    ```
+3. Install dependencies
+    ```bash
+    yarn install 
+    ```
+
+## Usage
+To start the development server and view the project in your browser, run the following command:
+```bash
+yarn dev
+```
+Visit http://localhost:5173/ in your browser
+
+## Configurations
+- You can change content of the website at `/data/user_info.js`
+- Change the Google Analytics MEASUREMENT ID at `index.html` to your own.
---
+++ src/data/user_info.js
@@ -0,0 +1,155 @@
+const info = {
+    // ============ MAIN DETAILS ============
+    main: {
+        name: "Mahimna M Mehta",
+        description: "Results-driven front-end developer with over 2 years of experience specializing in web and mobile app development. Proven ability to manage multiple projects and lead teams effectively. Currently seeking an internship + Job to leverage my skills in a dynamic environment and gain valuable industry experience.",
+        role: "Junior Front-End Developer",
+        photo: "../photo.jpeg",
+        email: "mahimna.mehta99@icloud.com",
+    },
+
+    // ============ SOCIAL LINKS ============
+    socials: {
+		github: "https://github.com/mahimnamehta",
+		linkedin: "https://linkedin.com/in/mahimnamehta",
+    },
+
+    // ============ PROJECTS ============
+    projects: [
+		{
+			title: "Friends Manager",
+			description: "Add or delete Friends - (React, Chakra UI, Emotion, framer-motion, vite, nodejs, mongodb)",
+			technologies: "React, Tailwind, TypeScript",
+			github: "https://github.com/mahimnamehta/react-python",
+            link: "https://react-python-2daq.onrender.com/"
+		},
+    ],
+
+    // ============ EDUCATION ============
+    education: [
+        {
+            school: "College Lasalle",
+            degree: "AEC Information Technology - Programmer & Analyst",
+            duration: "2023 - 2025",
+            image: "lasalle.png"
+        },
+        {
+            school: "Sal Institute of Technology & Research",
+            degree: "Computer Engineering",
+            duration: "2018 - 2022",
+            image: "sal.png"
+        },
+    ],
+
+    // ============ EXPERIENCE ============
+    experience: [
+        {
+            position: "Front-End Developer Internship",
+            company: "L'original",
+            duration: "May 2025 - Jun 2025 (3 Months)",
+            image: "loriginal.png",
+            descriptions: [
+                "On Going Internship in Wordpress Development",
+            ]
+        },
+        {
+            position: "Junior Front-end Developer",
+            company: "Appstasy Pvt. Ltd..",
+            duration: "Sep 2022 - Nov 2023 (1 year & 3 Months)",
+            image: "appslogo.jpg",
+            descriptions: [
+                "Internship Program Along with my Bachelors"
+            ]
+        },
+        {
+            position: "Technical Internship",
+            company: "Silverwings Technology PVT LTD . Apprenticeship",
+            duration: "Sep 2018 - Sep 2022 (4 Years)",
+            image: "silvi.jpg",
+            descriptions: [
+                "Internship Program Along with my Bachelors"
+            ]
+        },
+    ],
+
+    // ============ CERTIFICATES ============
+    certificates: [
+        {
+            title: "Advance React",
+            description: "Provided by Coursera & Authorized by Meta",
+            icon: "meta",
+            link: "https://www.coursera.org/account/accomplishments/verify/AR5UACH43JXN",
+        },
+        {
+            title: "HTML & CSS in depth",
+            description: "Provided by Coursera & Authorized by Meta",
+            icon: "meta",
+            link: "https://www.coursera.org/account/accomplishments/verify/M643DGNS2XJL",
+        },
+        {
+            title: "Introduction to Front-End Development",
+            description: "Provided by Coursera & Authorized by Meta",
+            icon: "meta",
+            link: "https://www.coursera.org/account/accomplishments/verify/HG2TEZGR8S6R"
+        },
+        {
+            title: "Programming with JavaScript",
+            description: "Provided by Coursera & Authorized by Meta",
+            icon: "meta",
+            link: "https://www.coursera.org/account/accomplishments/verify/Z97BKMFQSYQ3"
+        },
+        {
+            title: "React Basics",
+            description: "Provided by Coursera & Authorized by Meta",
+            icon: "meta",
+            link: "https://www.coursera.org/account/accomplishments/verify/VP4RD8HY7L84"
+        },
+        {
+            title: "Version Control",
+            description: "Provided by Coursera & Authorized by Meta",
+            icon: "meta",
+            link: "https://www.coursera.org/account/accomplishments/verify/7PUEMUCXAXAE"
+        },
+        {
+            title: "Ultimate AWS Certified Solutions Architect Associate 2021",
+            description: "Provided Udemy",
+            icon: "udemy",
+            link: "https://udemy-certificate.s3.amazonaws.com/pdf/UC-59fedf7c-3cc3-49f5-8385-af68101fa88a.pdf"
+        },
+        {
+            title: "Flutter & Dart Development For Building iOS and Android Apps",
+            description: "Provided by Udemy",
+            icon: "udemy",
+            link: "https://www.udemy.com/certificate/UC-06ed3ee5-b5d8-40b3-a37a-f61f87c4c12e/"
+        },
+        {
+            title: "Front End Development Libraries",
+            description: "Provided by FreeCodeCamp",
+            icon: "freecodecamp",
+            link: "https://www.freecodecamp.org/certification/mahimnamehta/front-end-development-libraries"
+        },
+        {
+            title: "Legacy JavaScript Algorithms and Data Structures",
+            description: "Provided by FreeCodeCamp",
+            icon: "freecodecamp",
+            link: "https://www.hackerrank.com/certificates/efdbdd44eb7d"
+        },
+        {
+            title: "Responsive Web Design",
+            description: "Provided by FreeCodeCamp",
+            icon: "freecodecamp",
+            link: "https://www.freecodecamp.org/certification/mahimnamehta/responsive-web-design"
+        }
+    ],
+
+    // ============ CONTACT ============
+    contact: {
+        title: "Let's Get in Touch: Ways to Connect with Me",
+        description: "Thank you for your interest in getting in touch with me. I welcome your feedback, questions, and suggestions. If you have a specific question or comment, please feel free to email me directly at mahimna.mehta99@icloud.com. I make an effort to respond to all messages within 24 hours, although it may take me longer during busy periods. Finally, if you prefer to connect on social media, you can connect with me using the social media links below.",
+    },
+
+    // ============ FOOTER ============
+    footer: "© 2024 Mahimna M Mehta. All Rights Reserved"
+}
+
+export default info
